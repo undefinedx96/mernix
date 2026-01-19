be747c8109f14a5baec27f3eae21fc4eddd3aa67
TBS96 <9tbs6@proton.me>
2026-01-19 16:47:06 +0530

    feat: create `user.routes.js` for `registerUser`
    _*for image upload, we cant directly access `avatar` and `coverImage` from `req.body`. so for that, we need to return middleware that processes multiple files associated with the given form fields, and we did it in `user.routes.js` with `upload.fields([{}])` method before passing `registerUser`. THIS IS LIKE: WHILE POST METHOD EXECUTES, A MIDDLEWARE WILL EXECUTE => `upload.fields([{}])`, AND THEN THE `registerUser` WILL EXECUTE*_
    
    THIS MULTER MIDDLEWARE IS DONE BECAUSE, WHILE USER TRIES TO CREATE A USER A/C, THEN THE ASSOCIATED FILES SHOULD GET SUBMITTED TEMPORARILY ON LOCAL SERVER AND THEN GETS UPLOADED TO CLOUDINARY, AND NOT ONLY THE TEXT DATA TO BE SUBMITTED
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

---
+++ server/src/routes/user.routes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express'
+import { upload } from '../middlewares/multer.middleware.js'
+import { registerUser } from '../controllers/user.controller.js';
+
+const userRouter = Router();
+
+userRouter.route('/register').post(
+    upload.fields([
+        {
+            name: 'avatar',
+            maxCount: 1
+        },
+        {
+            name: 'coverImage',
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
+
+export default userRouter;
