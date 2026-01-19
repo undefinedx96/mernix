4cc3308bd939a30901282c8a2de6a65d2a7e4c4d
TBS96 <9tbs6@proton.me>
2026-01-19 16:21:23 +0530

    add `multer` middleware for file upload handling
    - create middleware fn `storage` from multer
    - we're using `multer.diskStorage()` and not `multer.memoryStorage()` to store temporary files as using diskstorage is much much safer to use than relying on RAM
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

---
+++ server/src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/temp');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const extension = file.originalname.split('.').pop();
+        cb(null, `${file.fieldname}-${uniqueSuffix}.${extension}`);
+    },
+});
+
+export const upload = multer({
+    storage: storage,
+});
