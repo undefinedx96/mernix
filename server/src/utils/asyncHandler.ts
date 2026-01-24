dd26724093775c725f0d345d7f35b2c1492d408a
TBS96 <9tbs6@proton.me>
2026-01-24 19:36:42 +0530

    refactor: update `asyncHandler` HOC to use `RequestHandler` type from Express
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- server/src/utils/asyncHandler.ts
+++ server/src/utils/asyncHandler.ts
@@ -1,4 +1,22 @@
-const asyncHandler = (requestHandlerFn) => {
+// import type { NextFunction, Request, Response } from "express";
+
+// const asyncHandler = (requestHandlerFn: (req: Request, res: Response, next: NextFunction) => Promise<void> | Promise<unknown> | void) => {
+//     return (req: Request, res: Response, next: NextFunction) => {
+//         Promise
+//         .resolve(requestHandlerFn(req, res, next))
+//         .catch((err) => next(err))
+//     }
+// };
+
+// export { asyncHandler };
+
+
+
+// ================ OR BETTER TO IMPORT 'RequestHandler' TYPE DIRECTLY FROM EXPRESS ================
+
+import type { RequestHandler } from 'express'
+
+const asyncHandler = (requestHandlerFn: RequestHandler): RequestHandler => {
     return (req, res, next) => {
         Promise
         .resolve(requestHandlerFn(req, res, next))
