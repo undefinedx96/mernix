2444b6dfb284e76b684b23cd73ca0cff2381b9df
TBS96 <9tbs6@proton.me>
2026-01-28 17:26:22 +0530

    create `patch` endpoint for `updateUserCoverImage` with `verifyJWT` and multer's `upload.single()` as middlewares
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { upload } from '../middlewares/multer.middleware.ts'
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshTheAccessToken, registerUser, updateAccountDetails, updateUserAvatar } from '../controllers/user.controller.ts'
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshTheAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from '../controllers/user.controller.ts'
 import { verifyJWT } from '../middlewares/auth.middleware.ts'
 
 const userRouter = Router();
@@ -25,5 +25,6 @@ userRouter.route('/change-current-password').post(verifyJWT, changeCurrentPasswo
 userRouter.route('/get-current-user').get(verifyJWT, getCurrentUser);
 userRouter.route('/update-account-details').patch(verifyJWT, updateAccountDetails);
 userRouter.route('/update-user-avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar);
+userRouter.route('/update-user-cover').patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage);
 
 export default userRouter;
