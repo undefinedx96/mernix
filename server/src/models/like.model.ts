32c156ad90dd1a4605a995275f06e32d6f870305
TBS96 <9tbs6@proton.me>
2026-02-07 12:29:38 +0530

    create model `likes` with typed interface for fields
    Signed-off-by: TBS96 <9tbs6@proton.me>

---
+++ server/src/models/like.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose'
+
+export interface ILike extends Document {
+    comment: mongoose.Types.ObjectId;
+    video: mongoose.Types.ObjectId;
+    likedBy: mongoose.Types.ObjectId;
+    tweet: mongoose.Types.ObjectId;
+}
+
+const likeSchema = new Schema<ILike>(
+    {
+        comment: {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        },
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: 'Video'
+        },
+        likedBy: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        tweet: {
+            type: Schema.Types.ObjectId,
+            ref: 'Tweet'
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Like = mongoose.model<ILike>('Like', likeSchema);
