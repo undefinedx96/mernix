c4e9ddff9e2264b52af8e2859c444caf7a01f595
TBS96 <9tbs6@proton.me>
2026-02-18 22:55:08 +0530

    create `post` endpoint for `toggleCommentLike` with `verifyJWT` as middleware
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- server/src/routes/like.routes.ts
+++ server/src/routes/like.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.ts';
-import { toggleVideoLike } from '../controllers/like.controller.ts';
+import { toggleCommentLike, toggleVideoLike } from '../controllers/like.controller.ts';
 
 const likeRouter = Router();
 
 likeRouter.use(verifyJWT);
 
 likeRouter.route('/toggle-video-like/:videoId').post(toggleVideoLike);
+likeRouter.route('/toggle-comment-like/:commentId').post(toggleCommentLike);
 
 export default likeRouter;
