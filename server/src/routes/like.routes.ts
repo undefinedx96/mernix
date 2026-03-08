cb2954a00a8506c272e9ee2db4772ae0d65c7be6
TBS96 <9tbs6@proton.me>
2026-03-08 20:55:26 +0530

    create `post` endpoint for `toggleTweetLike` with `verifyJWT` as middleware
    - **TODO:** needs testing after creating tweet controller
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- server/src/routes/like.routes.ts
+++ server/src/routes/like.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.ts';
-import { toggleCommentLike, toggleVideoLike } from '../controllers/like.controller.ts';
+import { toggleCommentLike, toggleTweetLike, toggleVideoLike } from '../controllers/like.controller.ts';
 
 const likeRouter = Router();
 
@@ -8,5 +8,6 @@ likeRouter.use(verifyJWT);
 
 likeRouter.route('/toggle-video-like/:videoId').post(toggleVideoLike);
 likeRouter.route('/toggle-comment-like/:commentId').post(toggleCommentLike);
+likeRouter.route('/toggle-tweet-like/:tweetId').post(toggleTweetLike);
 
 export default likeRouter;
