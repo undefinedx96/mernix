6e470c67b2efc8977c2222398e3a6ce86613893a
TBS96 <9tbs6@proton.me>
2026-03-09 12:26:42 +0530

    create `get` endpoint for `getLikedVideos` with `verifyJWT` as middleware
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- server/src/routes/like.routes.ts
+++ server/src/routes/like.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.ts';
-import { toggleCommentLike, toggleTweetLike, toggleVideoLike } from '../controllers/like.controller.ts';
+import { getLikedVideos, toggleCommentLike, toggleTweetLike, toggleVideoLike } from '../controllers/like.controller.ts';
 
 const likeRouter = Router();
 
@@ -9,5 +9,6 @@ likeRouter.use(verifyJWT);
 likeRouter.route('/toggle-video-like/:videoId').post(toggleVideoLike);
 likeRouter.route('/toggle-comment-like/:commentId').post(toggleCommentLike);
 likeRouter.route('/toggle-tweet-like/:tweetId').post(toggleTweetLike);
+likeRouter.route('/get-liked-videos').get(getLikedVideos);
 
 export default likeRouter;
