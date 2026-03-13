90fce4d2e5bd350a8fe9d1081ca9b84b826ce3f0
TBS96 <9tbs6@proton.me>
2026-03-13 21:23:11 +0530

    create `patch` endpoint for `updateComment` with `verifyJWT` as middleware in `comment.routes.ts`
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- server/src/routes/comment.routes.ts
+++ server/src/routes/comment.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.ts';
-import { addComment } from '../controllers/comment.controller.ts';
+import { addComment, updateComment } from '../controllers/comment.controller.ts';
 
 const commentRouter = Router();
 
 commentRouter.use(verifyJWT);
 
 commentRouter.route('/add-comment/:videoId').post(addComment);
+commentRouter.route('/update-comment/:commentId').patch(updateComment);
 
 export default commentRouter;
