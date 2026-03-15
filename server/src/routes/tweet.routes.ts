2d600077e6d2dc23b206d314b86eba6651034ca8
TBS96 <9tbs6@proton.me>
2026-03-15 18:59:39 +0530

    create `patch` endpoint for `updateTweet` with `verifyJWT` as middleware in `tweet.routes.ts`
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- server/src/routes/tweet.routes.ts
+++ server/src/routes/tweet.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.ts';
-import { createTweet } from '../controllers/tweet.controller.ts';
+import { createTweet, updateTweet } from '../controllers/tweet.controller.ts';
 
 const tweetRouter = Router();
 
 tweetRouter.use(verifyJWT);
 
 tweetRouter.route('/create-tweet').post(createTweet);
+tweetRouter.route('/update-tweet/:tweetId').patch(updateTweet);
 
 export default tweetRouter;
