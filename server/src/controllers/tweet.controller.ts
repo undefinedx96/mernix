180ddfd52cb311c18302a4a9a0e6f5bd28102678
TBS96 <9tbs6@proton.me>
2026-03-15 18:58:42 +0530

    feat: implement `updateTweet` controller
    1. extract `tweetId` and `content` from `req.params` and `req.body` with types `TweetBody` and `TweetParams` respectively and validate them
    2. check whether content length exceeds 280 characters, else throw error `400`
    3. find if the `tweetId` and the `owner` exists, if exists, then update `content` with new `content` and populate it with `owner` details: `username`, `avatar`, `firstName`, `lastName`, using `findOneAndUpdate()`
    4. check if tweet has been updated or not, else throw error `404`
    5. return success response
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- server/src/controllers/tweet.controller.ts
+++ server/src/controllers/tweet.controller.ts
@@ -1,9 +1,10 @@
 import type { Request, Response } from 'express';
 import { asyncHandler } from '../utils/asyncHandler.ts';
-import type { TweetBody } from '../types/types.ts';
+import type { TweetBody, TweetParams } from '../types/types.ts';
 import { ApiError } from '../utils/ApiError.ts';
 import { Tweet } from '../models/tweet.model.ts';
 import { ApiResponse } from '../utils/ApiResponse.ts';
+import { isValidObjectId } from 'mongoose';
 
 
 
@@ -45,6 +46,53 @@ const createTweet = asyncHandler(async (req: Request<{}, {}, TweetBody>, res: Re
 
 
 
+
+const updateTweet = asyncHandler(async(req: Request<{}, {}, TweetBody>, res: Response) => {
+    const { tweetId } = req.params as TweetParams;
+    const { content } = req.body;
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, 'Invalid or missing tweet ID');
+    }
+
+    if (!content?.trim()) {
+        throw new ApiError(400, 'Tweet content is required');
+    }
+
+    if (content.trim().length > 280) {
+        throw new ApiError(400, 'Tweet content cannot exceed 280 characters');
+    }
+
+    const updatedTweet = await Tweet.findOneAndUpdate(
+        {
+            _id: tweetId,
+            owner: req.user?._id
+        },
+        {
+            $set: {
+                content: content.trim()
+            }
+        },
+        {
+            new: true
+        }
+    ).populate('owner', 'username avatar firstName lastName');
+
+    if (!updatedTweet) {
+        throw new ApiError(404, 'Tweet not found or unauthorized to edit');
+    }
+    // console.log('Updated tweet: ', updatedTweet);
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, updatedTweet, 'Tweet updated successfully')
+    );
+});
+
+
+
 export {
     createTweet,
+    updateTweet,
 }
