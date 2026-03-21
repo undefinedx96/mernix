8c3b1bd209d4c814590f0cdabbedbeb0c669e5c9
TBS96 <9tbs6@proton.me>
2026-03-21 21:57:53 +0530

    create `get` endpoint for `getUserTweets` with `verifyJWT` as middleware in `tweet.routes.ts`
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- server/src/routes/tweet.routes.ts
+++ server/src/routes/tweet.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.ts';
-import { createTweet, deleteTweet, updateTweet } from '../controllers/tweet.controller.ts';
+import { createTweet, deleteTweet, getUserTweets, updateTweet } from '../controllers/tweet.controller.ts';
 
 const tweetRouter = Router();
 
@@ -9,5 +9,6 @@ tweetRouter.use(verifyJWT);
 tweetRouter.route('/create-tweet').post(createTweet);
 tweetRouter.route('/update-tweet/:tweetId').patch(updateTweet);
 tweetRouter.route('/delete-tweet/:tweetId').delete(deleteTweet);
+tweetRouter.route('/get-user-tweets/:userId').get(getUserTweets);
 
 export default tweetRouter;
