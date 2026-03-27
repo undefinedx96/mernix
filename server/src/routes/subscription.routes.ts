608c529b7708d2c8746d79d30041d7591a8e1e7d
TBS96 <9tbs6@proton.me>
2026-03-27 21:18:10 +0530

    create `get` endpoint for `getUserChannelSubscribers` with `verifyJWT` as middleware in `subscription.routes.ts`
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- server/src/routes/subscription.routes.ts
+++ server/src/routes/subscription.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.ts';
-import { toggleSubscription } from '../controllers/subscription.controller';
+import { getUserChannelSubscribers, toggleSubscription } from '../controllers/subscription.controller';
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.use(verifyJWT);
 
 subscriptionRouter.route('/toggle-subscription/:channelId').post(toggleSubscription);
+subscriptionRouter.route('/get-user-channel-subs/:channelId').get(getUserChannelSubscribers);
 
 export default subscriptionRouter;
