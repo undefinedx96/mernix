6c1690a59969f111965c4d3e6170bf56e356f9e2
undefinedx96 <9tbs6@proton.me>
2026-04-03 21:24:29 +0530

    create `patch` endpoint for `removeVideoFromPlayList` with `verifyJWT` as middleware in `playlist.routes.ts`
    Signed-off-by: undefinedx96 <9tbs6@proton.me>

--- server/src/routes/playlist.routes.ts
+++ server/src/routes/playlist.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.ts';
-import { addVideoToPlaylist, createPlayList } from '../controllers/playlist.controller.ts';
+import { addVideoToPlaylist, createPlayList, removeVideoFromPlaylist } from '../controllers/playlist.controller.ts';
 
 const playlistRouter = Router();
 
@@ -8,5 +8,6 @@ playlistRouter.use(verifyJWT);
 
 playlistRouter.route('/create-playlist').post(createPlayList);
 playlistRouter.route('/add-video-to-playlist/:playlistId/:videoId').patch(addVideoToPlaylist);
+playlistRouter.route('/remove-video-from-playlist/:playlistId/:videoId').patch(removeVideoFromPlaylist);
 
 export default playlistRouter;
