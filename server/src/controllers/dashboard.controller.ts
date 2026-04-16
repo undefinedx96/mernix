4c8709f402850868d4fe6db803c097fb9a9fdc58
undefinedx96 <9tbs6@proton.me>
2026-04-16 22:07:13 +0530

    feat: implement `getChannelStats` controller
    1. extract `userId` from `req.user._id`, validate it and throw `401` and `404` errors respectively
    2. run parallel database queries using `Promise.all` to fetch video stats(`totalVideos` and `views`), **subscriber count**, and **total likes**
    3. inside `videoStats` aggregation, `match` by `owner` and `group` to `sum` up total videos and total views
    4. inside `totalSubscribers` aggregation, count total documents in `subscriptions` collection using `countDocuments` query where `channel` matches `userId`
    5. inside `totalLikes` aggregation, `lookup` **video** details, `unwind` them, `match` by **video owner,** and **count**
    6. extract values safely from the returned arrays and provide defaults (0) for new channels, with typed interface as `ChannelStatsResponse`
    7. return success response with `allStats` object
    
    Signed-off-by: undefinedx96 <9tbs6@proton.me>

---
+++ server/src/controllers/dashboard.controller.ts
@@ -0,0 +1,102 @@
+import type { Request, Response } from 'express';
+import { asyncHandler } from '../utils/asyncHandler.ts';
+import { ApiError } from '../utils/ApiError.ts';
+import mongoose, { isValidObjectId } from 'mongoose';
+import { Video } from '../models/video.model.ts';
+import { Subscription } from '../models/subscription.model.ts';
+import { Like } from '../models/like.model.ts';
+import { ApiResponse } from '../utils/ApiResponse.ts';
+import type { ChannelStatsResponse } from '../types/types.ts';
+
+
+
+
+const getChannelStats = asyncHandler(async (req: Request, res: Response) =>{
+    const userId = req.user?._id;
+
+    if (!userId) {
+        throw new ApiError(401, 'Unauthenticated unauthorized request');
+    }
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, 'Invalid or missing user ID');
+    }
+
+    const [videoStats, totalSubscribers, totalLikes] = await Promise.all([
+        Video.aggregate([
+            {
+                $match: {
+                    owner: new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalVideos: {
+                        $sum: 1
+                    },
+                    totalViews: {
+                        $sum: '$views'
+                    }
+                }
+            }
+        ]),
+
+        Subscription.countDocuments({
+            channel: userId
+        }),
+
+        Like.aggregate([
+            {
+                $lookup: {
+                    from: 'videos',
+                    localField: 'video',
+                    foreignField: '_id',
+                    as: 'videoDetails'
+                }
+            },
+            {
+                $unwind: '$videoDetails'
+            },
+            {
+                $match: {
+                    'videoDetails.owner': new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalLikesCount: {
+                        $sum: 1
+                    }
+                }
+            }
+        ])
+    ]);
+
+    const allStats: ChannelStatsResponse = {
+        totalVideos: videoStats[0]?.totalVideos || 0,
+        totalViews: videoStats[0]?.totalViews || 0,
+        subscribers: totalSubscribers || 0,
+        totalLikes: totalLikes[0]?.totalLikesCount || 0
+    };
+
+    // console.log('Video stats: ', videoStats);
+    // console.log('--------------------------------');
+    // console.log('Total subscribers: ', totalSubscribers);
+    // console.log('--------------------------------');
+    // console.log('Total likes: ', totalLikes);
+    // console.log('--------------------------------');
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, allStats, 'Channel stats fetched successfully')
+    );
+});
+
+
+
+export {
+    getChannelStats,
+}
