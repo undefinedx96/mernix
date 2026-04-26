1984226ffa85a2b7ce8f766cc56721e52d241abd
undefinedx96 <9tbs6@proton.me>
2026-04-26 17:30:33 +0530

    feat: implement centralized error handling middleware and update app configuration
    - return errors as json response, instead of express's default html structure
    
    Signed-off-by: undefinedx96 <9tbs6@proton.me>

---
+++ server/src/middlewares/error.middleware.ts
@@ -0,0 +1,32 @@
+import type { NextFunction, Request, Response } from 'express';
+import { ApiError } from '../utils/ApiError.ts';
+
+const errorHandler = (
+    err: unknown,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (err instanceof ApiError) {
+        // console.error('API Error Stack: ', err.stack);
+
+        return res
+        .status(err.statusCode)
+        .json({
+            statusCode: err.statusCode,
+            success: err.success,
+            message: err.message,
+            errors: err.errors,
+            data: err.data,
+        });
+    }
+
+    console.error('Unhandled Error: ', err);
+
+    return res.status(500).json({
+        success: false,
+        message: 'Internal Server Error',
+    });
+};
+
+export { errorHandler };
