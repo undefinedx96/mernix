b78d481c052c6206b035666065b3cee26d5687eb
undefinedx96 <9tbs6@proton.me>
2026-05-18 20:59:16 +0530

    fix(Sidebar & DashboardLayout): change initial sidebar state to closed and improve sidebar toggle functionality
    - auto collapse sidebar on screens below 1024px
    
    Signed-off-by: undefinedx96 <9tbs6@proton.me>

--- client/src/components/layouts/DashboardLayout.tsx
+++ client/src/components/layouts/DashboardLayout.tsx
@@ -6,22 +6,21 @@ import Sidebar from '../layout/Sidebar.tsx'
 
 
 const DashboardLayout = () => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+	return (
+		<div className='flex flex-col h-screen overflow-hidden'>
+			<Navbar onMenuClick={() => setIsSidebarOpen(!isSidebarOpen)} />
 
-  return (
-    <div className='flex flex-col h-screen overflow-hidden'>
-        <Navbar onMenuClick={() => setIsSidebarOpen(!isSidebarOpen)} />
+			<div className='flex flex-1 overflow-hidden'>
+				<Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
 
-        <div className='flex flex-1 overflow-hidden'>
-            <Sidebar isOpen={isSidebarOpen} />
-
-            <main className='flex-1 overflow-y-auto bg-zinc-50 dark:bg-zinc-950/50 scroll-smooth'>
-                <Outlet />
-            </main>
-        </div>
-    </div>
-  )
+				<main className='flex-1 overflow-y-auto bg-zinc-50 dark:bg-zinc-950/50 scroll-smooth w-full'>
+					<Outlet />
+				</main>
+			</div>
+		</div>
+	)
 }
 
 export default DashboardLayout
