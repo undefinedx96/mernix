4e9d1a95e3b5a4ea1b5a35280883727089b7953a
undefinedx96 <9tbs6@proton.me>
2026-08-03 19:33:18 +0530

    server/refactor(like): add validation middleware to like routes
    Signed-off-by: undefinedx96 <9tbs6@proton.me>

--- server/src/routes/like.routes.ts
+++ server/src/routes/like.routes.ts
@@ -1,14 +1,21 @@
-import { Router } from 'express';
-import { verifyJWT } from '../middlewares/auth.middleware.ts';
-import { getLikedVideos, toggleCommentLike, toggleTweetLike, toggleVideoLike } from '../controllers/like.controller.ts';
+import { Router } from 'express'
+import { verifyJWT } from '../middlewares/auth.middleware.ts'
+import { getLikedVideos, toggleCommentLike, toggleTweetLike, toggleVideoLike } from '../controllers/like.controller.ts'
+import { validate } from '../middlewares/validate.middleware.ts'
+import { getAllVideosQuerySchema, videoIdParamSchema } from '../validators/video.validator.ts'
+import { commentParamsSchema } from '../validators/comment.validator.ts'
+import { tweetParamsSchema } from '../validators/tweet.validator.ts'
 
 const likeRouter = Router();
 
 likeRouter.use(verifyJWT);
 
-likeRouter.route('/toggle-video-like/:videoId').post(toggleVideoLike);
-likeRouter.route('/toggle-comment-like/:commentId').post(toggleCommentLike);
-likeRouter.route('/toggle-tweet-like/:tweetId').post(toggleTweetLike);
-likeRouter.route('/get-liked-videos').get(getLikedVideos);
+likeRouter.route('/toggle-video-like/:videoId').post(validate({ params: videoIdParamSchema }), toggleVideoLike);
+
+likeRouter.route('/toggle-comment-like/:commentId').post(validate({ params: commentParamsSchema }), toggleCommentLike);
+
+likeRouter.route('/toggle-tweet-like/:tweetId').post(validate({ params: tweetParamsSchema }), toggleTweetLike);
+
+likeRouter.route('/get-liked-videos').get(validate({ query: getAllVideosQuerySchema }), getLikedVideos);
 
 export default likeRouter;
