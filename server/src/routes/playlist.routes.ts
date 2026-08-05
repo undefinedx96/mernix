2f94b6a994744b917c115437868e8cd8660c2d53
undefinedx96 <9tbs6@proton.me>
2026-08-05 21:03:50 +0530

    server/refactor(playlist): add validation middleware to playlist routes
    Signed-off-by: undefinedx96 <9tbs6@proton.me>

--- server/src/routes/playlist.routes.ts
+++ server/src/routes/playlist.routes.ts
@@ -1,17 +1,39 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.ts';
 import { addVideoToPlaylist, createPlayList, deletePlaylist, getPlaylistById, getUserPlaylists, removeVideoFromPlaylist, updatePlaylist } from '../controllers/playlist.controller.ts';
+import { validate } from '../middlewares/validate.middleware.ts';
+import { playlistBodySchema, playlistParamsSchema } from '../validators/playlist.validator.ts';
+import { tweetUserParamsSchema } from '../validators/tweet.validator.ts';
+import { getAllVideosQuerySchema } from '../validators/video.validator.ts';
 
 const playlistRouter = Router();
 
 playlistRouter.use(verifyJWT);
 
-playlistRouter.route('/create-playlist').post(createPlayList);
-playlistRouter.route('/add-video-to-playlist/:playlistId/:videoId').patch(addVideoToPlaylist);
-playlistRouter.route('/remove-video-from-playlist/:playlistId/:videoId').patch(removeVideoFromPlaylist);
-playlistRouter.route('/update-playlist/:playlistId').patch(updatePlaylist);
-playlistRouter.route('/delete-playlist/:playlistId').delete(deletePlaylist);
-playlistRouter.route('/get-playlist-by-id/:playlistId').get(getPlaylistById);
-playlistRouter.route('/get-user-playlists/:userId').get(getUserPlaylists);
+playlistRouter.route('/create-playlist').post(validate({ body: playlistBodySchema }), createPlayList);
+
+playlistRouter.route('/add-video-to-playlist/:playlistId/:videoId').patch(validate({ params: playlistParamsSchema }), addVideoToPlaylist);
+
+playlistRouter.route('/remove-video-from-playlist/:playlistId/:videoId').patch(validate({ params: playlistParamsSchema }), removeVideoFromPlaylist);
+
+playlistRouter.route('/update-playlist/:playlistId').patch(
+    validate({
+        params: playlistParamsSchema,
+        body: playlistBodySchema
+    }),
+    updatePlaylist
+);
+
+playlistRouter.route('/delete-playlist/:playlistId').delete(validate({ params: playlistParamsSchema }), deletePlaylist);
+
+playlistRouter.route('/get-playlist-by-id/:playlistId').get(validate({ params: playlistParamsSchema }), getPlaylistById);
+
+playlistRouter.route('/get-user-playlists/:userId').get(
+    validate({
+        params: tweetUserParamsSchema,
+        query: getAllVideosQuerySchema
+    }),
+    getUserPlaylists
+);
 
 export default playlistRouter;
