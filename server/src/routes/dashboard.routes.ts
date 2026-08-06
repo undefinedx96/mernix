bb5715527e039d8ce01a756ef0295b0eeb880097
undefinedx96 <9tbs6@proton.me>
2026-08-06 19:39:47 +0530

    server/refactor(dashboard): add validation middleware to dashboard routes
    Signed-off-by: undefinedx96 <9tbs6@proton.me>

--- server/src/routes/dashboard.routes.ts
+++ server/src/routes/dashboard.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.ts';
 import { getChannelStats, getChannelVideos } from '../controllers/dashboard.controller.ts';
+import { validate } from '../middlewares/validate.middleware.ts';
+import { getAllVideosQuerySchema } from '../validators/video.validator.ts';
 
 const dashboardRouter = Router();
 
 dashboardRouter.use(verifyJWT);
 
 dashboardRouter.route('/get-channel-stats').get(getChannelStats);
-dashboardRouter.route('/get-channel-videos').get(getChannelVideos);
+
+dashboardRouter.route('/get-channel-videos').get(validate({ query: getAllVideosQuerySchema }), getChannelVideos);
 
 export default dashboardRouter;
